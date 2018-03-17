2890e2f1d85dc28c4a1bdd13f8536f9fd90a3140
iotsharing.com <nha.tuan84@gmail.com>
2018-03-17 19:56:59 +0700

    Update regularization_linear_regression.py

--- regularization_linear_regression.py
+++ regularization_linear_regression.py
@@ -1,3 +1,4 @@
+#we use polynominal h(x) = theta0*x0 + theta1*x1 + theta2*x2 + theta3*x3
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -12,6 +13,7 @@ x = np.array([-0.99768,
 	0.47742,
 	0.82229])
 m = len(x)
+#generate y values x^2-10x+25
 y = np.array([2.0885,
 	1.1646,
 	0.3287,
@@ -46,7 +48,7 @@ while (True):
         preJ = J
         
     tmp = alpha*x_train.dot(error)/m
-    tmp2 = tmp[1::,0] - alpha*lamda*theta[1::,0]/m
+    tmp2 = tmp[1::,0] + alpha*lamda*theta[1::,0]/m
     theta[0,0::] = theta[0,0::] - tmp[0,0::]
     theta[1::,0] = theta[1::,0] - tmp2
 
