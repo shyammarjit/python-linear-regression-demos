cab4155d4e92c9f2c032a1cf0e42de1156e66068
iotsharing.com <nha.tuan84@gmail.com>
2018-03-17 19:23:15 +0700

    Update regularization_linear_regression.py

--- regularization_linear_regression.py
+++ regularization_linear_regression.py
@@ -1,4 +1,3 @@
-#we use polynominal h(x) = theta0*x0 + theta1*x1 + theta2*x2 + theta3*x3
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,7 +12,6 @@ x = np.array([-0.99768,
 	0.47742,
 	0.82229])
 m = len(x)
-#generate y values x^2-10x+25
 y = np.array([2.0885,
 	1.1646,
 	0.3287,
